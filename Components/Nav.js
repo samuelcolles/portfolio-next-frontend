ff330a526383ded1ecb169b14a6d1cf851182ec3
samuelcolles <samueltaziocolles@hotmail.com>
2022-08-12 16:52:11 -0700

    Reactivity Fixes

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,17 +1,17 @@
 import Link from 'next/link'
-import styles from '../styles/nav.module.scss'
+import style from '../styles/nav.module.scss'
 
 const Nav = () => {
     return (
         <div className="background-box background-color-a">
-            <nav className={ styles.nav+" container"}>
+            <nav className={style.nav + " container"}>
                 <h1 className="okay">Samuel Colles</h1>
-                <Link href='/'>About</Link>
-                <Link href='/resume'>Resume</Link>
-                {/* <Link href='/blog'>Blog</Link> */}
-                <Link href='/contact'>Contact</Link>
-
-
+                <div className={style.linkShelf}>
+                    <Link href='/'>About</Link>
+                    <Link href='/resume'>Resume</Link>
+                    {/* <Link href='/blog'>Blog</Link> */}
+                    <Link href='/contact'>Contact</Link>
+                </div>
             </nav>
         </div>
 
