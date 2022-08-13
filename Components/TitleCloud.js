267494300e37496875501225665f8c99ed66b73f
samuelcolles <samueltaziocolles@hotmail.com>
2022-08-12 17:21:14 -0700

    More Reactivity fixes

--- Components/TitleCloud.js
+++ Components/TitleCloud.js
@@ -3,10 +3,9 @@ import Style from '../styles/titleCloud.module.scss'
 
 const TitleCloud = () => {
     return <div className={Style.root}>
-        <div className={Style.container}>
+
             <img className={Style.cloud + " " + Style.cloud1} src="/clouds/WebCloud1.svg" />
             <img className={Style.cloud + " " + Style.cloud2} src="/clouds/WebCloud2.svg" />
-        </div>
     </div>
 }
 
