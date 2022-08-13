89a3f8fd25a992c3dd36f070e96f4e806b453344
samuelcolles <samueltaziocolles@hotmail.com>
2022-08-13 00:01:27 -0700

    deployment fix

--- Components/CertificationCard.js
+++ Components/CertificationCard.js
@@ -5,10 +5,11 @@ import Link from "next/link"
 export default function CertificationCard({ certification }) {
     const { badge, link } = certification.attributes
     const badgeImageURL = badge.data[0].attributes.url
+    console.log(badgeImageURL)
     return <Link href={link} >
         <div className={style.certification}>
             <div className="box">
-                <Image src={badgeImageURL} height={300} width={300} />
+                <img src={badgeImageURL} height={300} width={300} />
             </div>
         </div>
 
