46364e222950e8b869e1c9cb425ba06bd9669205
samuelcolles <samueltaziocolles@hotmail.com>
2022-08-14 02:29:42 -0700

    added console log

--- Components/CertificationCard.js
+++ Components/CertificationCard.js
@@ -1,11 +1,12 @@
 import style from "../styles/certificationCard.module.scss"
 import Image from "next/image"
 import Link from "next/link"
+import { useEffect } from "react"
 
 export default function CertificationCard({ certification }) {
     const { badge, link } = certification.attributes
     const badgeImageURL = badge.data[0].attributes.url
-    console.log(badgeImageURL)
+    useEffect(()=>console.log(badgeImageURL),[])
     return <Link href={link} >
         <div className={style.certification}>
             <div className="box">
