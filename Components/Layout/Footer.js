76203b1ddb1fedb146610034bd997e13aaa07fc8
samuelcolles <samueltaziocolles@hotmail.com>
2022-08-17 04:15:16 -0700

    added success page and footer mail icon

--- Components/Layout/Footer.js
+++ Components/Layout/Footer.js
@@ -1,15 +1,16 @@
 import Style from './footer.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-export default function Footer({copyRight}){
+export default function Footer({ copyRight }) {
     return <footer className={Style.root + " background-box background-color-a"}>
-        <div className={Style.content+" container"}>
+        <div className={Style.content + " container"}>
             <p>Ⓒ Samuel Colles 2022</p>
             <div className={Style.iconBox}>
-
-            <a href='https://www.linkedin.com/in/samuel-colles/'><FontAwesomeIcon icon={faLinkedin}/></a>
-            <a href='https://github.com/samuelcolles'><FontAwesomeIcon icon={faGithub}/></a>
+                <a href='mailto:samueltaziocolles@hotmail.com'><FontAwesomeIcon icon={faEnvelope} /></a>
+                <a href='https://www.linkedin.com/in/samuel-colles/'><FontAwesomeIcon icon={faLinkedin} /></a>
+                <a href='https://github.com/samuelcolles'><FontAwesomeIcon icon={faGithub} /></a>
             </div>
         </div>
     </footer>
--- pages/success.js
+++ pages/success.js
@@ -2,8 +2,9 @@ import React from 'react'
 import Style from '../styles/success.module.scss'
 
 const success = () => {
-    return <div className="background-box">
-        <div className={Style.root + " container"}>
+    return <div className={Style.root + " background-box"}>
+        <div className={Style.box + " container"}>
+            <h1>Thank you!</h1>
             <p>Thank you for taking the time to contact me! I will get back to you as soon as possible.</p>
         </div>
     </div>
