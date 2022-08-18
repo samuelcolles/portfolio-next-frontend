17df6c3dc932490ab9add167088f955a15a5b17d
samuelcolles <samueltaziocolles@hotmail.com>
2022-08-17 22:29:07 -0700

    minor cloud improvements

--- Components/BackgroundClouds/BackgroundClouds.js
+++ Components/BackgroundClouds/BackgroundClouds.js
@@ -8,10 +8,10 @@ const BackgroundClouds = () => {
         <img className={Style.cloud + " " + Style.cloud2} src="/clouds/SmallCloud2.svg" />
         <img className={Style.cloud + " " + Style.cloud3} src="/clouds/MediumCloud2.svg" />
         <img className={Style.cloud + " " + Style.cloud4} src="/clouds/MediumCloud1.svg" />
-        <img className={Style.cloud + " " + Style.cloud5} src="/clouds/MediumCloud2.svg" />
+        <img className={Style.cloud + " " + Style.cloud5} src="/clouds/MediumCloud3.svg" />
         <img className={Style.cloud + " " + Style.cloud6} src="/clouds/LargeCloud1.svg" />
-        {/* <img className={Style.cloud + " " + Style.cloud7} src="/clouds/SmallCloud1.svg" />
-        <img className={Style.cloud + " " + Style.cloud8} src="/clouds/SmallCloud1.svg" /> */}
+        <img className={Style.cloud + " " + Style.cloud7} src="/clouds/SmallCloud3.svg" />
+        <img className={Style.cloud + " " + Style.cloud8} src="/clouds/MediumCloud4.svg" />
     </div>
   )
 }
