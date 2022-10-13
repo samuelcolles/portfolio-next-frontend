fbf7f7a80cfe3382d74b3d9673a6e6f3feda1c19
samuelcolles <samueltaziocolles@hotmail.com>
2022-10-12 18:16:07 -0700

    A couple fixes to demoless projects

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,29 @@
-import ProjectCard from '../Components/ProjectCard/ProjectCard'
-import TitleCloud from '../Components/TitleCloud/TitleCloud'
+import ProjectCard from "../Components/ProjectCard/ProjectCard";
+import TitleCloud from "../Components/TitleCloud/TitleCloud";
 
 export default function Home({ projects }) {
-
-  return <>
-    <TitleCloud />
-    <div className='background-box'>
-      <div className='grid-1-col container'>
-        {
-          projects.map((project, index) => <ProjectCard key={index} project={project} />)
-        }
+  return (
+    <>
+      <TitleCloud />
+      <div className="background-box">
+        <div className="grid-1-col container">
+          {projects.map((project, index) => (
+            <ProjectCard key={index} project={project} />
+          ))}
+        </div>
       </div>
-    </div>
-  </>
+    </>
+  );
 }
 export const getStaticProps = async () => {
-  const resProjects = await fetch(process.env.STRAPI_BACKEND_URL + '/api/projects?populate=*')
-  const projects = await resProjects.json()
+  const resProjects = await fetch(
+    process.env.STRAPI_BACKEND_URL + "/api/projects?populate=*&sort=id"
+  );
+  const projects = await resProjects.json();
 
   return {
     props: {
       projects: projects.data,
-    }
-  }
-}
+    },
+  };
+};
